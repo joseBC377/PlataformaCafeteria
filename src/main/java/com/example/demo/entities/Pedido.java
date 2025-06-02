7f067a0fc5ab7f6a63d714145e09f4f9645f75be
JoseBC <jbalcazar377@gmail.com>
2025-06-02 00:06:10 -0500

    entidad-pedido

--- src/main/java/com/example/demo/entities/Pedido.java
+++ src/main/java/com/example/demo/entities/Pedido.java
@@ -1,7 +1,17 @@
 package com.example.demo.entities;
 
+import java.time.LocalDate;
+
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +21,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Table(name="Pedido")
 public class Pedido {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String fecha;
+    
+    @Column(nullable=false)
+    @NotNull(message = "La fecha es obligartoria")
+    private LocalDate fecha;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario",nullable = false)
+    @NotNull(message = "El id_usuario es obligatorio")
     private Usuario usuario;
 
 }
